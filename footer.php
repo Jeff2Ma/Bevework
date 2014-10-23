e67fb6ddfc5941e94036e7d919874e1040f5cc9b
Jeff Ma <mjh2316@gmail.com>
2014-10-23 13:59:07 +0800

    页脚基本完成

--- footer.php
+++ footer.php
@@ -22,26 +22,27 @@
 	<div class="f-content">
 		<div class="float_left">
 			<div class="social">
-				<a href=""><i class="icon-weibo"></i><span>新浪微博</span></a>
-				<a href=""><i class="icon-twitter"></i><span>Twitter</span></a>
-				<a href=""><i class="icon-github"></i><span>Github</span></a>
+				<a href="" class="weibo"><i class="icon-weibo"></i><span>新浪微博</span></a>
+				<a href="" class="qq"><i class="icon-qq"></i><span>QQ</span></a>
+				<a href="" class="twitter"><i class="icon-twitter"></i><span>Twitter</span></a>
+				<a href="" class="github"><i class="icon-github"></i><span>Github</span></a>
 			</div>
-			<p class="center">专业数码评测媒体 您身边的数码评测</p>
+			<p class="subtitle center">专业数码评测媒体 您身边的数码评测</p>
 		</div><!-- .float_left -->
 		<div class="float_right">
 			<div class="weixin"><img src="<?php bloginfo('template_url'); ?>/images/wechatimg.png" alt=""><span>微信</span></div>
 			<div class="about">
 				<ul>
-					<li>DW商城</li>
-					<li>DW商城</li>
-					<li>DW商城</li>
-					<li>DW商城</li>
+					<li><a href="">DW商城</a></li>
+					<li><a href="">DW商城</a></li>
+					<li><a href="">DW商城</a></li>
+					<li><a href="">DW商城</a></li>
 				</ul>
 				<ul>
 					<li>关于我们</li>
-					<li>联系我们</li>
-					<li>DW商城</li>
-					<li>DW商城</li>
+					<li><a href="">DW商城</a></li>
+					<li><a href="">DW商城</a></li>
+					<li><a href="">DW商城</a></li>
 				</ul>
 			</div>
 			<div class="cpright"></div>
@@ -49,7 +50,7 @@
 	</div> <!-- .f-content -->
 
 
-	<p class="clearfix">Powered by <a target="_blank" rel="nofollow" href="">WordPress</a>. Designed from <a  target="_blank" rel="nofollow" href="" title="安卓网">anzhuo</a>. Theme by <a  target="_blank" rel="nofollow" href="" title="DeveWork.com荣誉出品">Bevework</a>.</p>
+	<p class="clearfix footercpright">&copy; <?php echo date("Y")?>  <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>.  Powered by <a target="_blank" rel="nofollow" href="">WordPress</a>. Designed from <a  target="_blank" rel="nofollow" href="" title="安卓网">anzhuo</a>. Theme by <a  target="_blank" rel="nofollow" href="" title="DeveWork.com荣誉出品">Bevework</a>.</p>
 	
 </footer>
 
